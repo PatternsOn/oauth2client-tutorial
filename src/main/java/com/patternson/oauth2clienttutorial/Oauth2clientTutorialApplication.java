e6004a3199b3e0e6c8fbe0e5ddb4f23e66430a66
TobiasPettersson <pettersson.tobias84@gmail.com>
2018-01-21 21:24:51 +0100

    init commit

---
+++ src/main/java/com/patternson/oauth2clienttutorial/Oauth2clientTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.patternson.oauth2clienttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2clientTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2clientTutorialApplication.class, args);
+	}
+}
---
+++ src/test/java/com/patternson/oauth2clienttutorial/Oauth2clientTutorialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.patternson.oauth2clienttutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2clientTutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
